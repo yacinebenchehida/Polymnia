b1251ca6bd6243ed2fd9d7f497ded2dd2fa15a33
yacinebenchehida <yacine.ben.chehida@hotmail.fr>
2025-09-29 18:16:40 +0100

    Update blast_uniprot.sh

--- blast_uniprot.sh
+++ blast_uniprot.sh
@@ -1,29 +1,41 @@
+#!/bin/bash
+
+CHR=$1
+START=$2
+END=$3
+OUTPUT_DIR=$4
+DB_PATH=$5
+
 REF="/mnt/scratch/projects/biol-specgen-2018/yacine/Polymnia/Data/Gene_color_genes"
 FASTA_FILE="GCA_965197365.1_ilMecPoly1.hap1.1_genomic.fna"
-RESULTS="Polymnia_$1_$2_$3"
 
-mkdir -p $RESULTS
+mkdir -p "$OUTPUT_DIR"
 
-(echo ">$1"; cat $REF/$FASTA_FILE|awk '/^>/ { if(NR>1) print "";  printf("%s\n",$0); next; } { printf("%s",$0);}  END {printf("\n");}'|grep -A1 $1|grep -v $1| cut -c "$2"-"$3") > $RESULTS/"$1"_"$2"_"$3".fasta
+# Extract sequence region
+(echo ">$CHR"; 
+ cat $REF/$FASTA_FILE | awk '/^>/ { if(NR>1) print ""; printf("%s\n",$0); next; } { printf("%s",$0);} END {printf("\n"); }' \
+ | grep -A1 "$CHR" | grep -v "$CHR" | cut -c "$START"-"$END") > "$OUTPUT_DIR/${CHR}_${START}_${END}.fasta"
 
-echo FASTA FILE READY
+echo "FASTA FILE READY"
 
+# Run Augustus
 module load AUGUSTUS/3.5.0-foss-2023a
-augustus --progress=true --strand=both --species=heliconius_melpomene1 $RESULTS/"$1"_"$2"_"$3".fasta > $RESULTS/"$1"_"$2"_"$3".gff
+augustus --progress=true --strand=both --species=heliconius_melpomene1 \
+  "$OUTPUT_DIR/${CHR}_${START}_${END}.fasta" > "$OUTPUT_DIR/${CHR}_${START}_${END}.gff"
 
-echo AUGUSTUS FINISHED RUNNING 
+echo "AUGUSTUS FINISHED RUNNING"
 
-getAnnoFasta.pl  $RESULTS/"$1"_"$2"_"$3".gff
-echo PROTEIN FASTA READY
+# Extract protein fasta
+getAnnoFasta.pl "$OUTPUT_DIR/${CHR}_${START}_${END}.gff"
+echo "PROTEIN FASTA READY"
 
-module purge 
+# Run BLAST
+module purge
 module load BLAST+/2.14.1-gompi-2023a
 
-echo STARTING BLASTP
-blastp -query $RESULTS/"$1"_"$2"_"$3".aa -db database_insect -outfmt 6 -max_target_seqs 5 | awk '$12 > 80' > $RESULTS/"Polymnia_$1_$2_$3"_blastp_gff_genes_against_uniprot.txt
-echo BLASTP DONE
+echo "STARTING BLASTP"
+blastp -query "$OUTPUT_DIR/${CHR}_${START}_${END}.aa" \
+  -db "$DB_PATH" \
+  -outfmt 6 -max_target_seqs 5 | awk '$12 > 80' > "$OUTPUT_DIR/Polymnia_${CHR}_${START}_${END}_blastp_results.txt"
 
-
-
-
-# USAGE = ./blast_uniprot.sh  ctg002890_1 1030001 1260001
+echo "BLASTP DONE"
